3786c6b4005d7847e20e8c79a8e292bac12d5992
Pizzosta <pizzostor@gmail.com>
2023-01-29 11:37:45 -0800

    a class Square that defines a square by: (based on 4-square.py)

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -40,5 +40,6 @@ class Square:
             print()
         else:
             for i in range(self.__size):
-                [print("#", end="") for j in range(self.__size)]
-            print()
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
