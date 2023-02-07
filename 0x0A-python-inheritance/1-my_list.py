3a5d58ca229bb78b9f90b7d41189a1200a35ddce
Pizzosta <pizzostor@gmail.com>
2023-02-07 15:14:51 -0800

    a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a class MyList that inherits from list"""
+
+
+class MyList(list):
+    """Returns list sorted in ascending order
+
+    Args:
+        list(int) - elements of the list
+    """
+    def __init__(self):
+        """ Initialize MyList Object"""
+    super().__init__()
+
+    def print_sorted(self):
+        """Returns sorted list"""
+        print(sorted(self))
