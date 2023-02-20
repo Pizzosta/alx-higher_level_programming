657b36b97d7e7f689ead6ecf39f73eff4ce5cb44
Pizzosta <pizzostor@gmail.com>
2023-02-20 14:27:20 -0800

    the class Rectangle that inherits from Base

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,11 +17,11 @@ class Rectangle(Base):
             id(int): id of the rectangle
         """
 
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
         @property
         def width(self):
