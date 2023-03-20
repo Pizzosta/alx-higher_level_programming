395ee1c912fb45118486deff1937228ebb00f390
Pizzosta <pizzostor@gmail.com>
2023-03-20 08:16:34 -0700

    a script that lists all records of the table second_table of the database hbtn_0c_0

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+--  lists all records of the table in a database
+-- Query lists all records of the table second_table of the database hbtn_0c_0
+SELECT score, name FROM second_table
+ORDER BY score DESC, name ASC;
