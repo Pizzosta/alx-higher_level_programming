fd461b14df814f44eea030b3716db3ccbbab468a
Pizzosta <pizzostor@gmail.com>
2023-03-22 16:11:41 -0700

    script that creates the database hbtn_0d_usa and the table cities

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- a script that creates the database hbtn_0d_usa and the table cities
+-- creates database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use database
+USE hbtn_0d_usa;
+-- creates table
+CREATE TABLE IF NOT EXISTS cities(
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	state_id INT NOT NULL FOREIGN KEY REFERENCES state(id),
+	name VARCHAR(256) NOT NULL);
