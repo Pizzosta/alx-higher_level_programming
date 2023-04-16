cc3546fbf3c3c557a74b0f2a3cfdeb579c45632b
Pizzosta <pizzostor@gmail.com>
2023-04-16 10:17:19 -0700

    a script that searches the second biggest integer in the list of arguments

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/* A script that searches the second biggest integer 
+ * in the list of argument
+ * If no argument passed, print 0
+ * If the number of arguments is 1, print 0
+ */
+
+if (process.argv.length <= 2) {
+	return (0);
+} else {
+	const args = process.argv.slice(2);
+	const integers = args.map(Number);
+	const sortedIntegers = integers.sort((a, b) => b - a);
+	console.log(sortedIntegers[1]);
+}
+
