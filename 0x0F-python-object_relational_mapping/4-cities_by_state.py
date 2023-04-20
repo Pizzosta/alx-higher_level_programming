d683c779197f2b18d2c7018b96afe2386b549139
Pizzosta <pizzostor@gmail.com>
2023-04-20 16:53:51 -0700

    a script that lists all cities from the database

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" a script that takes in an argument and displays
+all values in the states table of the DB
+where name matches the argument and made safe from SQL injection"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    """Prevent script from working when function is imported"""
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         user=mysql_username,
+                         passwd=mysql_password,
+                         db=database_name,
+                         port=3306)
+
+    # create cursor
+    cur = db.cursor()
+
+    # define SQL query
+    query = "SELECT cities.id, cities.name, states.name FROM cities \
+            INNER JOIN states ON states.id = cities.states.id"
+
+    # Execute query
+    cur.execute(query, ())
+
+    cities = cur.fetchall()
+
+    # print results
+    for city in cities:
+        print(city)
+
+    # close all connections/clean-up
+    cur.close()
+    db.close()
