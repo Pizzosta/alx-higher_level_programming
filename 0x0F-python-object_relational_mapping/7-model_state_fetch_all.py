e505c1b2cc6cf751a0312849a78ebcc2ca2af6c5
Pizzosta <pizzostor@gmail.com>
2023-04-21 05:45:03 -0700

    a script that lists all State objects from the database

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,7 +9,7 @@ from model_state import Base, State
 if __name__ == '__main__':
     # create new engine and session
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3])
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
     # generate database schema
     Base.metadata.create_all(engine)
