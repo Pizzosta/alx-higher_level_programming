96befb1964a77348de7efaa84419588ec3275233
Pizzosta <pizzostor@gmail.com>
2023-04-21 05:38:45 -0700

    a script that lists all State objects from the database

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,9 +12,12 @@ if __name__ == '__main__':
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
+    # generate database schema
+    Base.metadata.create_all(engine)
+
+    # create a new session
     Session = sessionmaker(bind=engine)
     session = Session()
-    Base.metadata.create_all(engine)
 
     # Execute query
     states = session.query(State).order_by(State.id).all()
