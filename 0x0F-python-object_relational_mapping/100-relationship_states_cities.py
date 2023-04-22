063312cb77128d8389358e980dd6b03e03768e18
Pizzosta <pizzostor@gmail.com>
2023-04-22 07:47:11 -0700

    Improve the files model_city.py and model_state.py, and save them as relationship_city.py and relationship_state.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -5,12 +5,14 @@ a name containing the letter a from the database """
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from relationship_state import Base, State
+from relationship_city import City
 
 if __name__ == '__main__':
     # create new engine and session
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
     # generate database schema
     Base.metadata.create_all(engine)
