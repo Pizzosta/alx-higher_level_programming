609317b8082fde536ca176eb9308cc61be69a1e8
Pizzosta <pizzostor@gmail.com>
2023-04-22 07:17:49 -0700

    Improving the files model_city.py and model_state.py, and save them as relationship_city.py and relationship_state.py

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" a script that deletes all State objects with
+a name containing the letter a from the database """
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == '__main__':
+    # create new engine and session
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    # generate database schema
+    Base.metadata.create_all(engine)
+
+    # create a new session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # query
+    new_state = State.(name="California")
+    new_city = City.(name="San Francisco")
+    new_state.cities.append(new_city)
+
+    # insert the new State and City object
+    session.add(new_state)
+    session.add(new_city)
+
+    # commit session
+    session.commit()
+
+    # close session
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Create a db schema using SQLAlchemy
+The script will create a class definition of City and an instance
+'Base = declarative_base()'
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from relationship_state import Base
+
+
+class City(Base):
+    """Creates an instance of City
+
+    Args:
+        id(int): id of city
+        name(str): name of city
+        state_id(int): id of State where City is located
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=False,
+                unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
