ab140189ef4ebfeb99c2aac46ee766a808e2c06b
Pizzosta <pizzostor@gmail.com>
2023-05-01 09:15:59 -0700

    cURL body size of a URL

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a request to that URL, 
-# and displays the size of the body of the response
-
+# a Bash script displays the size of a URL in bytes
 curl -sSL "$1" | wc -c
 
