f3d1c053909045427f0b0be99f0ffa0cda5ac3c6
Pizzosta <pizzostor@gmail.com>
2023-05-02 09:44:50 -0700

    a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,8 +5,7 @@ import requests
 
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    response = requests.get(url)
+    response = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}", type(response.text))
-    print("\t- content: {}", response.text)
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
