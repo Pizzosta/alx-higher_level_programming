fbf5d830a76ac254b9d03016c23ad10490419e61
Pizzosta <pizzostor@gmail.com>
2023-05-24 23:44:51 +0000

    a script that display the status code of a GET request.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+* The first argument is the URL to request (GET)
+* The status code must be printed like this: code: <status code>
+* You must use the module request
+* usage: ./2-statuscode.js <url>
+*/
+
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
